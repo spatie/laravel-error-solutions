48739cb192f93dfea14ded4813b010705c28617a
freekmurze <483853+freekmurze@users.noreply.github.com>
2025-03-08 07:13:47 +0000

    Fix styling

--- tests/SpatieRendererTest.php
+++ tests/SpatieRendererTest.php
@@ -6,5 +6,5 @@ use Spatie\LaravelErrorSolutions\Tests\TestClasses\ExceptionWithSolution;
 it('will render a solution set a on a throwable', function () {
     $renderer = app(Renderer::class);
 
-    $html = $renderer->render(request(), new ExceptionWithSolution());
+    $html = $renderer->render(request(), new ExceptionWithSolution);
 })->skip('Could not figure out where view could not be found in tests');
